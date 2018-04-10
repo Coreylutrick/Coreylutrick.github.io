382562d4fe54371078693ed79d83a8f688e352dc
Corey Lutrick <coreylutrick@gmail.com>
2018-04-10 16:25:51 -0700

    changed functions formats

--- project.js
+++ project.js
@@ -1,55 +1,11 @@
-// var projects = [
-//   {
-//     id: "project1", 
-//     title: "Prokect 2 Headed Snake", 
-//     imageUrl: "../main/2snake.jpg", 
-//     description: "This is the best project",
-//     githubUrl: "https://github.com/Coreylutrick/Coreylutrick.github.io"
-//     },
-//     {
-//       id: "project2", 
-//       title: "Project Turtle on the Moon", 
-//       imageUrl: "../main/space-turtle.jpg", 
-//       description: "This is the best project",
-//       githubUrl: "https://github.com/Coreylutrick/Coreylutrick.github.io"
-//     },
-//     {
-//       id: "project3", 
-//       title: "Project Footwear", 
-//       imageUrl: "../main/pigBoots.jpg", 
-//       description: "This is the best project",
-//       githubUrl: "https://github.com/Coreylutrick/Coreylutrick.github.io"
-//     },
-//     {
-//       id: "project4", 
-//       title: "Project Spaghetti", 
-//       imageUrl: "../main/spag.jpg", 
-//       description: "This is the best project",
-//       githubUrl: "https://github.com/Coreylutrick/Coreylutrick.github.io"
-//     },
-//     {
-//       id: "project5", 
-//       title: "Project Saliva", 
-//       imageUrl: "../main/tungboy.jpg", 
-//       description: "This is the best project",
-//       githubUrl: "https://github.com/Coreylutrick/Coreylutrick.github.io"
-//     },
-//     {
-//       id: "project6", 
-//       title: "Project Doom", 
-//       imageUrl: "../main/doom.jpg", 
-//       description: "This is the best project",
-//       githubUrl: "https://github.com/Coreylutrick/Coreylutrick.github.io"
-//     }
-// ];
 
-function printToDom(stringToPrint, divId)
+const printToDom = (stringToPrint, divId) =>
 {
     var divElement = document.getElementById(divId);
     divElement.innerHTML += stringToPrint;
 }
 
-function buildDomString(project)
+const buildDomString = (project) =>
 {
     var domString = ""
       for (var i = 0; i < project.length; i++)
